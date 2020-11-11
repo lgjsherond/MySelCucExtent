abf3e40f3c4a43017836adb868aa6867acd5c2d8
lgjsherond <50234665+lgjsherond@users.noreply.github.com>
2020-11-11 14:56:26 +0530

    Remove unwanted codes

--- src/test/java/com/demo/stepdefinitions/googleDef.java
+++ src/test/java/com/demo/stepdefinitions/googleDef.java
@@ -19,20 +19,6 @@ public class googleDef {
     Scenario scenario;
     String screenshotdir = System.getProperty("user.dir") + "/target/Screenshots";
 
-//    public String getBase64Screenshot(WebDriver driver) throws IOException {
-//        String Base64StringofScreenshot="";
-//        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-//        // for saving screenshots in local - this is optional
-//        Date oDate = new Date();
-//        SimpleDateFormat oSDF = new SimpleDateFormat("ddMMYYYY_HHmmss");
-//        String sDate = oSDF.format(oDate);
-//        FileUtils.copyFile(src, new File(screenshotdir + "Screenshot_" + sDate + ".png"));
-//        //
-//        byte[] fileContent = FileUtils.readFileToByteArray(src);
-//        Base64StringofScreenshot = "data:image/png;base64," + Base64.getEncoder().encodeToString(fileContent);
-//        return Base64StringofScreenshot;
-//    }
-
     @Given("Launch google page")
     public void launchGooglePage() {
         WebDriverManager.chromedriver().setup();
@@ -50,15 +36,12 @@ public class googleDef {
     }
 
     @AfterStep
-    public void attach_screenshot() throws Throwable {
+    public void attach_screenshot() {
         if(scenario.isFailed()){
             TakesScreenshot ts=(TakesScreenshot)driver;
             byte[] screenshot=ts.getScreenshotAs(OutputType.BYTES);
-//            scenario.log("This is my failure message");
             scenario.attach(screenshot,"image/png","");
         }
-
-//        ExtentCucumberAdapter.addTestStepScreenCaptureFromPath(getBase64Screenshot(driver)); //for html
     }
 
     @After
@@ -71,7 +54,5 @@ public class googleDef {
         driver.findElement(By.name("q")).sendKeys("Sheron");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
         Assert.assertEquals(driver.getTitle(),"Sheron - Gogle Search");
-//        scenario.embed(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES), "image/png");
-
     }
 }
